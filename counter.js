8cfd1fdd76b921fbf1e72c7ebc8393601b7d6145
cloudworknepal5 <cloudworknepal5@gmail.com>
2026-01-12 21:27:12 +0545

    Update counter.js

--- counter.js
+++ counter.js
@@ -1,6 +1,6 @@
 /**
- * Name: counter.js (The Universal Fix)
- * Features: Multi-function Digit Converter, Object-to-Number Parser, Refresh Guard
+ * Name: counter.js (Force Start & Fix)
+ * Version: 10.0
  */
 
 const BloggerCounter = {
@@ -9,56 +9,48 @@ const BloggerCounter = {
         numMap: {'0':'०','1':'१','2':'२','3':'३','4':'४','5':'५','6':'६','7':'७','8':'८','9':'९'}
     },
 
-    // १. नेपाली अंकमा बदल्ने फङ्सन
     toNepali: function(num) {
         let n = parseInt(num) || 0;
         return n.toString().split('').map(char => this.config.numMap[char] || char).join('');
     },
 
-    // २. मुख्य फङ्सन (Update & Fetch)
     updateAndFetch: async function(elementId) {
         const el = document.getElementById(elementId);
         const postId = window.location.pathname.replace(/[/.]/g, "_");
         const url = `${this.config.databaseURL}/views/${postId}.json`;
         
-        // सेसन चेक (रिफ्रेस हुँदा संख्या बढ्न नदिन)
+        // सेसन चेक
         const hasVisited = sessionStorage.getItem("v_" + postId);
 
         try {
-            // डेटाबेसबाट डेटा तान्ने
             const response = await fetch(url);
             const data = await response.json();
             
-            // --- [OBJECT OBJECT] निवारण ---
+            // डेटाको अवस्था जाँच गर्ने
             let count = 0;
             if (data !== null) {
-                if (typeof data === 'number') {
-                    count = data;
-                } else if (typeof data === 'object') {
-                    // यदि डेटाबेसमा {value: 1} जस्तो अब्जेक्ट छ भने त्यसबाट १ निकाल्ने
-                    count = parseInt(Object.values(data)[0]) || 0;
-                } else {
-                    count = parseInt(data) || 0;
-                }
+                if (typeof data === 'number') count = data;
+                else if (typeof data === 'object') count = parseInt(Object.values(data)[0]) || 0;
+                else count = parseInt(data) || 0;
             }
 
-            // यदि नयाँ भिजिटर हो भने १ थप्ने र डेटाबेसमा पठाउने
+            // यदि पहिलो पटक आएको हो भने र संख्या ० छ भने १ बनाउने
             if (!hasVisited) {
-                count += 1;
+                count = (count === 0) ? 1 : count + 1;
+                
                 await fetch(url, {
                     method: 'PUT',
-                    body: JSON.stringify(count) // केवल शुद्ध नम्बर मात्र पठाउने
+                    body: JSON.stringify(count)
                 });
                 sessionStorage.setItem("v_" + postId, "true");
             }
 
-            // नतिजा देखाउने (आइकन सहित)
             if (el) {
                 const eyeIcon = `<svg style="width:16px; height:16px; margin-right:6px; fill:#ce1212; vertical-align:middle;" viewBox="0 0 576 512"><path d="M288 32c-80.8 0-145.5 36.8-192.6 80.6C48.6 156 17.3 208 2.5 243.7c-3.3 7.9-3.3 16.7 0 24.6C17.3 304 48.6 356 95.4 399.4 142.5 443.2 207.2 480 288 480s145.5-36.8 192.6-80.6c46.8-43.4 78.1-95.4 92.9-131.1 3.3-7.9 3.3-16.7 0-24.6C558.7 208 527.4 156 480.6 112.6 433.5 68.8 368.8 32 288 32zM144 256a144 144 0 1 1 288 0 144 144 0 1 1 -288 0zm144-64a64 64 0 1 0 0 128 64 64 0 1 0 0-128z"/></svg>`;
                 el.innerHTML = `${eyeIcon} <span style="font-weight:700;">${this.toNepali(count)} पटक हेरियो</span>`;
             }
         } catch (error) {
-            console.error("Counter Error:", error);
+            console.error("Fetch Error:", error);
         }
     }
 };
