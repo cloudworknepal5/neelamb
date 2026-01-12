3f3f9522fa4aa7101288719401f133d81989dcd1
cloudworknepal5 <cloudworknepal5@gmail.com>
2026-01-12 21:34:56 +0545

    Update counter.js

--- counter.js
+++ counter.js
@@ -1,6 +1,6 @@
 /**
- * Name: counter.js (Ultimate Refresh Fix)
- * Version: 11.0
+ * Name: counter.js (The Perfect Refresh Fix)
+ * Version: 12.0
  */
 
 const BloggerCounter = {
@@ -9,50 +9,49 @@ const BloggerCounter = {
         numMap: {'0':'०','1':'१','2':'२','3':'३','4':'४','5':'५','6':'६','7':'७','8':'८','9':'९'}
     },
 
+    // १. नेपाली अंकमा बदल्ने फङ्सन (Multi-function Helper)
     toNepali: function(num) {
         let n = parseInt(num) || 0;
         return n.toString().split('').map(char => this.config.numMap[char] || char).join('');
     },
 
+    // २. मुख्य फङ्सन: डाटा ल्याउने र अपडेट गर्ने
     updateAndFetch: async function(elementId) {
         const el = document.getElementById(elementId);
         const postId = window.location.pathname.replace(/[/.]/g, "_");
         const url = `${this.config.databaseURL}/views/${postId}.json`;
         
-        // सेसन चेक (केवल 'Update' रोक्नका लागि, 'Fetch' रोक्न होइन)
+        // सेसन चेक (रिफ्रेस गर्दा डाटाबेसमा संख्या नबढोस् भन्नका लागि)
         const hasVisited = sessionStorage.getItem("v_" + postId);
 
         try {
-            // १. पहिले डेटाबेसबाट डाटा तान्ने (रिफ्रेस गर्दा पनि यो चल्छ)
+            // क) पहिले डाटाबेसबाट हालको डाटा तान्ने (यसले रिफ्रेस गर्दा ० देखिन दिँदैन)
             const response = await fetch(url);
-            let data = await response.json();
-            
-            let count = 0;
-            if (data !== null) {
-                if (typeof data === 'number') count = data;
-                else if (typeof data === 'object') count = parseInt(Object.values(data)[0]) || 0;
-                else count = parseInt(data) || 0;
+            let count = await response.json();
+
+            // डाटा शुद्धिकरण (यदि डाटाबेस खाली छ वा गलत छ भने ० मान्ने)
+            if (count === null || typeof count !== 'number') {
+                count = 0;
             }
 
-            // २. यदि यो नयाँ सेसन हो भने मात्र १ थप्ने र डेटाबेसमा पठाउने
+            // ख) यदि यो नयाँ सेसन हो भने मात्र संख्या १ ले बढाउने
             if (!hasVisited) {
-                count = count + 1;
+                count += 1;
                 await fetch(url, {
                     method: 'PUT',
-                    headers: { 'Content-Type': 'application/json' },
                     body: JSON.stringify(count)
                 });
                 sessionStorage.setItem("v_" + postId, "true");
             }
 
-            // ३. डिस्प्ले गर्ने (यो जहिले पनि चल्छ)
+            // ग) आइकनसहित सही संख्या प्रदर्शन गर्ने
             if (el) {
-                const eyeIcon = `<svg style="width:16px; height:16px; margin-right:6px; fill:#ce1212; vertical-align:middle;" viewBox="0 0 576 512"><path d="M288 32c-80.8 0-145.5 36.8-192.6 80.6C48.6 156 17.3 208 2.5 243.7c-3.3 7.9-3.3 16.7 0 24.6C17.3 304 48.6 356 95.4 399.4 142.5 443.2 207.2 480 288 480s145.5-36.8 192.6-80.6c46.8-43.4 78.1-95.4 92.9-131.1 3.3-7.9 3.3-16.7 0-24.6C558.7 208 527.4 156 480.6 112.6 433.5 68.8 368.8 32 288 32zM144 256a144 144 0 1 1 288 0 144 144 0 1 1 -288 0zm144-64a64 64 0 1 0 0 128 64 64 0 1 0 0-128z"/></svg>`;
-                el.innerHTML = `${eyeIcon} <span style="font-weight:700; color:#333;">${this.toNepali(count)} पटक हेरियो</span>`;
+                const eyeIcon = `<svg style="width:18px; height:18px; margin-right:6px; fill:#ce1212; vertical-align:middle;" viewBox="0 0 576 512"><path d="M288 32c-80.8 0-145.5 36.8-192.6 80.6C48.6 156 17.3 208 2.5 243.7c-3.3 7.9-3.3 16.7 0 24.6C17.3 304 48.6 356 95.4 399.4 142.5 443.2 207.2 480 288 480s145.5-36.8 192.6-80.6c46.8-43.4 78.1-95.4 92.9-131.1 3.3-7.9 3.3-16.7 0-24.6C558.7 208 527.4 156 480.6 112.6 433.5 68.8 368.8 32 288 32zM144 256a144 144 0 1 1 288 0 144 144 0 1 1 -288 0zm144-64a64 64 0 1 0 0 128 64 64 0 1 0 0-128z"/></svg>`;
+                el.innerHTML = `${eyeIcon} <span style="font-weight:700;">${this.toNepali(count)} पटक हेरियो</span>`;
             }
         } catch (error) {
-            console.error("Firebase Error:", error);
-            if(el) el.innerHTML = "० पटक हेरियो";
+            console.error("Counter Error:", error);
+            if (el) el.innerHTML = "० पटक हेरियो";
         }
     }
 };
