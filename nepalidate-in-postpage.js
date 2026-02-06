911f7dc55adcdf7f2c1b3d69ea4d80da43424ae8
cloudworknepal5 <cloudworknepal5@gmail.com>
2026-02-06 17:24:45 +0545

    Update nepalidate-in-postpage.js

--- nepalidate-in-postpage.js
+++ nepalidate-in-postpage.js
@@ -1,7 +1,7 @@
 /**
  * Name: nepalidate-in-postpage.js
- * Version: 8.0 (Precision + Weekdays)
- * Feature: Multi-function logic with Weekday support for 2082/2026
+ * Version: 8.3 (Correction Only)
+ * Target: Feb 6, 2026 = Magh 23, 2082
  */
 
 (function() {
@@ -11,18 +11,18 @@
         numMap: {'0':'०','1':'१','2':'२','3':'३','4':'४','5':'५','6':'६','7':'७','8':'८','9':'९'},
         weekdays: ['आइतबार', 'सोमबार', 'मंगलबार', 'बुधबार', 'बिहीबार', 'शुक्रबार', 'शनिबार'],
         monthData: {
-            'January':   { m: 'माघ', offset: 56, start: 15 }, 
-            'February':  { m: 'फागुन', offset: 56, start: 13 },
-            'March':     { m: 'चैत', offset: 56, start: 15 },
-            'April':     { m: 'वैशाख', offset: 57, start: 14 },
-            'May':       { m: 'जेठ', offset: 57, start: 15 },
-            'June':      { m: 'असार', offset: 57, start: 15 },
-            'July':      { m: 'साउन', offset: 57, start: 17 },
-            'August':    { m: 'भदौ', offset: 57, start: 17 },
-            'September': { m: 'असोज', offset: 57, start: 17 },
-            'October':   { m: 'कात्तिक', offset: 57, start: 18 },
-            'November':  { m: 'मंसिर', offset: 57, start: 17 },
-            'December':  { m: 'पुस', offset: 57, start: 16 }
+            'January':   { m: 'माघ', offset: 56, start: 15, prevDays: 16 }, 
+            'February':  { m: 'फागुन', offset: 56, start: 13, prevDays: 17 },
+            'March':     { m: 'चैत', offset: 56, start: 15, prevDays: 14 },
+            'April':     { m: 'वैशाख', offset: 57, start: 14, prevDays: 17 },
+            'May':       { m: 'जेठ', offset: 57, start: 15, prevDays: 17 },
+            'June':      { m: 'असार', offset: 57, start: 15, prevDays: 16 },
+            'July':      { m: 'साउन', offset: 57, start: 17, prevDays: 16 },
+            'August':    { m: 'भदौ', offset: 57, start: 17, prevDays: 16 },
+            'September': { m: 'असोज', offset: 57, start: 17, prevDays: 15 },
+            'October':   { m: 'कात्तिक', offset: 57, start: 18, prevDays: 14 },
+            'November':  { m: 'मंसिर', offset: 57, start: 17, prevDays: 14 },
+            'December':  { m: 'पुस', offset: 57, start: 16, prevDays: 15 }
         }
     };
 
@@ -31,10 +31,8 @@
 
     /** Function 2: Get Weekday Name */
     const getNepWeekday = (year, month, day) => {
-        // month index in JS Date is 0-11
-        const monthIndex = new Date(`${month} ${day}, ${year}`).getMonth();
-        const dayOfWeek = new Date(year, monthIndex, day).getDay();
-        return config.weekdays[dayOfWeek];
+        const d = new Date(`${month} ${day}, ${year}`);
+        return config.weekdays[d.getDay()];
     };
 
     /** Function 3: Calculate BS Year */
@@ -43,7 +41,7 @@
         return engYear + baseOffset;
     };
 
-    /** Function 4: Calculate BS Day and Month */
+    /** Function 4: Calculate BS Day and Month (Corrected Calculation) */
     const getBSDateDetails = (engDay, engMonth) => {
         const data = config.monthData[engMonth];
         let bsDay, bsMonth = data.m;
@@ -51,11 +49,11 @@
         if (engDay >= data.start) {
             bsDay = (engDay - data.start) + 1;
         } else {
-            // Roll back to previous Nepali month
             const months = ['पुस','माघ','फागुन','चैत','वैशाख','जेठ','असार','साउन','भदौ','असोज','कात्तिक','मंसिर'];
             let idx = months.indexOf(data.m);
             bsMonth = idx === 0 ? months[11] : months[idx - 1];
-            bsDay = (engDay + 30 - data.start) + 1; // Approx previous month end
+            // Correction applied here
+            bsDay = data.prevDays + engDay; 
         }
         return { bsDay, bsMonth };
     };
@@ -73,7 +71,6 @@
                 const bsYear = getBSYear(yInt, eMonth, dInt, config.monthData[eMonth].offset);
                 const weekday = getNepWeekday(yInt, eMonth, dInt);
 
-                // Format: आइतबार, माघ २, २०८२
                 el.innerHTML = `${weekday}, ${bsMonth} ${toNepNum(bsDay)}, ${toNepNum(bsYear)}`;
             }
         });
